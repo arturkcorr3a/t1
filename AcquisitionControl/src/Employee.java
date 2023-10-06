90811d71dc8ca6ad6927e9a8ce81e7dd8b5b604a
Artur Kalil <arturkcorr3a@gmail.com>
2023-10-06 20:16:19 -0300

    Developed java class 'Employee'

--- AcquisitionControl/src/Employee.java
+++ AcquisitionControl/src/Employee.java
@@ -3,8 +3,14 @@ public class Employee extends User {
 	private Department department;
 
 	public Employee(String name, Department department) {
-
+		super.name = name;
+		this.department = department;
+		super.id = 5000 + User.id_aux;
+		User.id_aux = 0;
 	}
 
-	//toString (override)
+	@Override
+	public String toString(){
+		return super.toString() + " | Department: " + this.department;
+	}
 }
