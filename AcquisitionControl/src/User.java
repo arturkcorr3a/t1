d0eee9abdaf76a7a4cfd8f299a7d2a606eec9413
Artur Kalil <arturkcorr3a@gmail.com>
2023-10-06 18:51:17 -0300

    Developed java class 'User' and updated the UML

--- AcquisitionControl/src/User.java
+++ AcquisitionControl/src/User.java
@@ -1,18 +1,39 @@
 public abstract class User {
 
 	protected String name;
-	protected int static id_aux = 1;
+	protected static int id_aux = 1;
 	protected int id;
 
 	public String inicials() {
-		return null;
+		if (name == null) return null;
+		String inicials = "" + this.name.charAt(0) + this.name.charAt(1);
+		return inicials.toUpperCase();
 	}
 
-	public String type() {
-		return null;
+	//true: Employee; false: Admin
+	public boolean type() {
+		if (this.id >= 5000) return true;
+		return false;
 	}
 
-	//getter
-	//setters
-	//toString
+	public String typeString(){
+		if(type()) return "Employee";
+		return "Administrator";
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String toString(){
+		return "Name: " + name + " | ID: " + this.id + "-" + inicials();
+	}
 }
